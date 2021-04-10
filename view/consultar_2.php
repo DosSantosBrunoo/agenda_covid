9856ef1c87b1bdb5ecf8a3246d8dddfa1b260d2b
Bruno <bruno.dossantos@anima.edu.uy>
2021-04-10 20:16:43 -0300

    arreglo en consulta

--- view/consultar_2.php
+++ view/consultar_2.php
@@ -18,18 +18,17 @@ head("Consultar por grupo")
                         <h1 style="text-align: center" class="d-flex flex-row justify-content-center">Consultar cantidad de usuarios agendados por grupo</h1>
                         <label style="margin-top: 15px;" class="navbar-brand" for="group">Seleccione grupo*</label>
                         <div class="d-flex justify-content-center">
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-                                <label class="form-check-label" for="inlineRadio1">Grupo 1</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-                                <label class="form-check-label" for="inlineRadio2">Grupo 2</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3">
-                                <label class="form-check-label" for="inlineRadio3">Grupo 3</label>
-                            </div>
+                            <select required class="form-select form-select-sm" aria-label=".form-select-sm example">
+                                <option value="" hidden>-</option>
+                                <option value="1">Sin grupo</option>
+                                <option value="2">Personal CTI</option>
+                                <option value="3">Hisopadores</option>
+                                <option value="4">Personal Salud General</option>
+                                <option value="5">Personal Educacion</option>
+                                <option value="6">Bomberos</option>
+                                <option value="7">Policia</option>
+                                <option value="8">Personal Residencia</option>
+                            </select>
                         </div>
                     </div>
                     <div class="form-group d-flex flex-row justify-content-center">
