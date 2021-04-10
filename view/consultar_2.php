8d4f4424b943193214b0eccda0cbac8288c0d0e6
Bruno <bruno.dossantos@anima.edu.uy>
2021-04-10 19:25:03 -0300

    consultar nashe

--- view/consultar_2.php
+++ view/consultar_2.php
@@ -10,11 +10,37 @@ head("Consultar por grupo")
     <?php
     include './partials/navbar.php'
     ?>
-    <form action="">
-
-
-
-    </form>
+    <div class="container-fluid">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-6 ">
+                <form style="margin-top: 120px" action="#" method="get">
+                    <div class="form-group">
+                        <h1 style="text-align: center" class="d-flex flex-row justify-content-center">Consultar cantidad de usuarios agendados por grupo</h1>
+                        <label style="margin-top: 15px;" class="navbar-brand" for="group">Seleccione grupo*</label>
+                        <div class="d-flex justify-content-center">
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                                <label class="form-check-label" for="inlineRadio1">Grupo 1</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                                <label class="form-check-label" for="inlineRadio2">Grupo 2</label>
+                            </div>
+                            <div class="form-check form-check-inline">
+                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3">
+                                <label class="form-check-label" for="inlineRadio3">Grupo 3</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group d-flex flex-row justify-content-center">
+                        <button type="submit" class="btn">Consultar</button>
+                    </div>
+                </form>
+                <?php
+                ?>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
